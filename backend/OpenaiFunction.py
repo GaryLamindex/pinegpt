e9683612a937beee03bdc6e0fd8e8b55bb5519e6
ss8-local-server <96756548+GaryLamindex@users.noreply.github.com>
2023-05-05 12:05:48 +0800

    1

--- backend/OpenaiFunction.py
+++ backend/OpenaiFunction.py
@@ -2,34 +2,24 @@ import openai
 import os
 import pandas as pd
 
-def getIntent(api_key: str, userInput: str) -> str:
-    prompt = f"""
-Please identify the intents of the following user inputs:
-
-1. User: How much is the price of Apple stock?
-   AI: Intent: normal_chat
-
-2. User: Can you provide the latest news on Tesla?
-   AI: Intent: normal_chat
-
-3. User: What is the backtest result of the strategy?
-   AI: Intent: strategy_backtest_result_enquiry
-
-4. User: {userInput}
-   AI: Intent: """
-
+def getIntent(api_key: str, user_message: str) -> str:
     openai.api_key = api_key
 
-    response = openai.Completion.create(
-        model='gpt-3.5-turbo',
-        prompt=prompt,
-        max_tokens=20,
-        n=1,
-        stop=["\n"],
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful assistant that classifies user intents.",
+            },
+            {"role": "user", "content": user_message},
+        ],
+        temperature=0.5,
     )
 
-    intent = response.choices[0].text.strip()
-    return intent
+    intent = response.choices[0].message['content'].strip().replace("assistant: ", "", 1)
+    return intent.lower()
+
 
 def handle_backtest_result_enquiry():
     df = pd.read_csv('backtest.csv')
